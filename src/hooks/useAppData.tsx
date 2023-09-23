21383fc07d7809f32627b0293625b6edff68ecab
RolandoDrRobot <rolando.arguello.email@gmail.com>
2023-09-22 22:07:25 -0600

    Tenemos BTC price en tiempo real

--- src/hooks/useAppData.tsx
+++ src/hooks/useAppData.tsx
@@ -1,32 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
-import axios from 'axios';
+import * as io from "socket.io-client";
+import { useEffect, useState } from "react";
 
 const useBTCPrice = () => {
   const [btcPrice, setBTCPrice] = useState(0);
-  const update = useCallback(async () => {
-    try {
-      await axios.get('http://localhost:443/fetchBtcPrice').then((response) => {
-        setBTCPrice(response.data);
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }, []);
-
+  const socket = io.connect("http://localhost:3001");
   useEffect(() => {
-    update();
-  }, [update]);
+    socket.on("receive_btc_price", (data) => {
+      console.log(data);
+      if (data !== null) setBTCPrice(data);
+    });
+  }, [socket]);
   return {
     btcPrice
   }
 }
 
 export { useBTCPrice }
-
-// const usePurchases = () => {
-  
-// }
-
-// const useTotals = () => {
-  
-// }
